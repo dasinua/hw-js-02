bf3005175503b33d9228098b2166abf50306a655
dasinua <dasinua@gmail.com>
2016-04-05 18:23:21 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -6,15 +6,15 @@ for ( var i = 0; i < 5; i++ ) {
 var userLogin = prompt( 'Введите логин', '' );
 for ( var n = 0; n < userNames.length; n++ ) {
 
-	if ( userLogin == userNames[n] ) {
+	if ( userLogin == userNames[n] && userLogin != null && userLogin != undefined && userLogin != "" ) {
 		alert(( userLogin +', ' +'вы успешно вошли'));
 		break;
 	}
 
 }
-if ( userLogin != userNames[n] ) {
+ if ( userLogin != userNames[n] || userLogin == null || userLogin == undefined || userLogin == ""  ) {
 	alert(('Ошибка входа'));
-}
+ }
 
 
 
